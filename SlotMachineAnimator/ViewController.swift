ca0a9cdbd0348d3380250aab984671087f88d887
Hobin Han <hobin1019@gmail.com>
2025-08-17 05:03:26 -0700

    init

---
+++ SlotMachineAnimator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SlotMachineAnimator
+//
+//  Created by Hobin Han on 8/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ SlotMachineAnimatorTests/SlotMachineAnimatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineAnimatorTests.swift
+//  SlotMachineAnimatorTests
+//
+//  Created by Hobin Han on 8/17/25.
+//
+
+import Testing
+@testable import SlotMachineAnimator
+
+struct SlotMachineAnimatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
